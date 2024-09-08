80a8c8e483d65e5d4277703e231198a9ce52334c
Adam Podoxin <adam.podoxin@gmail.com>
2024-09-07 21:05:40 -0700

    Add clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Assignment Tracker",
@@ -9,15 +10,17 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <main className="bg-background flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#021b6d] to-[#15162c] text-white">
-          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-            {children}
-          </div>
-        </main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body>
+          <main className="bg-background flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#021b6d] to-[#15162c] text-white">
+            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+              {children}
+            </div>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from "@clerk/nextjs";
+
 const HomePage = () => {
   return (
     <>
       <h1 className="text-2xl">Assignment Tracker</h1>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </>
   );
 };
