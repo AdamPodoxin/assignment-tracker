d47d3795686f7382c373a044a543393a3292f2dd
Adam Podoxin <adam.podoxin@gmail.com>
2024-09-07 21:09:35 -0700

    Add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
         <body>
-          <main className="bg-background flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#021b6d] to-[#15162c] text-white">
+          <main className="bg-background text-primary flex min-h-screen flex-col items-center justify-center">
             <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
               {children}
             </div>
