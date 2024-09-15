3dc7d9597f9d87186cc51eedceaa0880e7cf6a10
Adam Podoxin <adam.podoxin@gmail.com>
2024-09-15 16:47:16 -0700

    Status badges

--- src/app/semester/[id]/columns.tsx
+++ src/app/semester/[id]/columns.tsx
@@ -10,6 +10,13 @@ import {
 } from "~/components/ui/dropdown-menu";
 import { Button } from "~/components/ui/button";
 import { deleteAssignment } from "~/app/actions/semester";
+import { Badge } from "~/components/ui/badge";
+
+const toTitleCase = (s: string) =>
+  s.replace(
+    /\w\S*/g,
+    (text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase(),
+  );
 
 export const getColumns = ({ refetch }: { refetch: () => void }) => {
   const columns: ColumnDef<Assignment>[] = [
@@ -25,14 +32,38 @@ export const getColumns = ({ refetch }: { refetch: () => void }) => {
       accessorKey: "dueDate",
       header: "Due Date",
       cell: ({ row }) => {
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-        const dueDate = row.getValue("dueDate") as Date;
+        const dueDate = row.original.dueDate;
         return dueDate.toDateString();
       },
     },
     {
       accessorKey: "status",
       header: "Status",
+      cell: ({ row }) => {
+        const status = row.original.status;
+        const statusString = status
+          .toString()
+          .split("_")
+          .map(toTitleCase)
+          .join(" ");
+
+        switch (status) {
+          case "NOT_DONE":
+            return (
+              <Badge className="bg-red-600 text-white">{statusString}</Badge>
+            );
+
+          case "IN_PROGRESS":
+            return (
+              <Badge className="bg-amber-600 text-white">{statusString}</Badge>
+            );
+
+          case "DONE":
+            return (
+              <Badge className="bg-green-600 text-white">{statusString}</Badge>
+            );
+        }
+      },
     },
     {
       id: "actions",
