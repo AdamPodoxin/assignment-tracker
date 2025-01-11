efaf1e19a6658f71e5e67872b11cd920bca6fcb8
Adam Podoxin <adam.podoxin@gmail.com>
2025-01-10 23:29:17 -0800

    Editing using modal

--- src/app/semester/[id]/AssignmentsTable.tsx
+++ src/app/semester/[id]/AssignmentsTable.tsx
@@ -4,7 +4,7 @@ import { useMemo, useState } from "react";
 import { format } from "date-fns";
 import { useRouter } from "next/navigation";
 import { CalendarIcon, DownloadIcon, FileIcon, PlusIcon } from "lucide-react";
-import { type Assignment, Status } from "@prisma/client";
+import type { Assignment, Status } from "@prisma/client";
 import {
   type SortingState,
   type ColumnFiltersState,
@@ -29,15 +29,6 @@ import {
   TableRow,
 } from "~/components/ui/table";
 import { Button } from "~/components/ui/button";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "~/components/ui/select";
 import {
   Popover,
   PopoverContent,
@@ -72,25 +63,25 @@ export const AssignmentsTable = ({
 
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
+  const [isEditing, setIsEditing] = useState(false);
   const [editingAssignmentId, setEditingAssignmentId] = useState("");
-  const [editingAssignmentName, setEditingAssignmentName] = useState("");
-  const [editingAssignmentCourse, setEditingAssignmentCourse] = useState("");
-  const [editingAssignmentDate, setEditingAssignmentDate] = useState(
-    new Date(),
-  );
   const [editingAssignmentStatus, setEditingAssignmentStatus] =
-    useState<Status>(Status.NOT_DONE);
+    useState<Status>("NOT_DONE");
 
   const columns = useMemo(
     () =>
       getColumns({
         refetch,
         editAssignment: (assignment) => {
+          setIsEditing(true);
+
           setEditingAssignmentId(assignment.id);
-          setEditingAssignmentName(assignment.name);
-          setEditingAssignmentCourse(assignment.course);
-          setEditingAssignmentDate(assignment.dueDate);
+          setNewAssignmentName(assignment.name);
+          setNewAssignmentCourse(assignment.course);
+          setNewAssignmentDate(assignment.dueDate);
           setEditingAssignmentStatus(assignment.status);
+
+          setIsDialogOpen(true);
         },
       }),
     [refetch],
@@ -115,12 +106,14 @@ export const AssignmentsTable = ({
   const [newAssignmentLink, setNewAssignmentLink] = useState("");
   const [newAssignmentDate, setNewAssignmentDate] = useState(new Date());
 
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
   return (
     <div className="flex w-full flex-col gap-2">
       <span className="flex gap-2">
-        <Dialog>
+        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
           <DialogTrigger asChild>
-            <Button className="my-2">
+            <Button onClick={() => setIsEditing(false)} className="my-2">
               <span className="flex items-center">
                 <PlusIcon className="mr-2 h-4 w-4" /> Add assignment
               </span>
@@ -177,16 +170,30 @@ export const AssignmentsTable = ({
               <DialogClose asChild>
                 <Button
                   onClick={async () => {
-                    await addAssignment({
-                      assignment: {
-                        course: newAssignmentCourse,
-                        name: newAssignmentName,
-                        dueDate: newAssignmentDate,
-                        status: "NOT_DONE",
-                        link: newAssignmentLink || null,
-                      },
-                      semesterId: semester.id,
-                    });
+                    if (isEditing) {
+                      await editAssignment({
+                        semesterId: semester.id,
+                        assignmentId: editingAssignmentId,
+                        data: {
+                          course: newAssignmentCourse,
+                          name: newAssignmentName,
+                          link: newAssignmentLink,
+                          dueDate: newAssignmentDate,
+                          status: editingAssignmentStatus,
+                        },
+                      });
+                    } else {
+                      await addAssignment({
+                        assignment: {
+                          course: newAssignmentCourse,
+                          name: newAssignmentName,
+                          dueDate: newAssignmentDate,
+                          status: "NOT_DONE",
+                          link: newAssignmentLink || null,
+                        },
+                        semesterId: semester.id,
+                      });
+                    }
 
                     refetch();
                   }}
@@ -255,114 +262,114 @@ export const AssignmentsTable = ({
           <TableBody>
             {table.getRowModel().rows?.length ? (
               table.getRowModel().rows.map((row) => {
-                if (row.original.id === editingAssignmentId) {
-                  return (
-                    <TableRow key={row.original.id}>
-                      <TableCell>
-                        <Input
-                          placeholder="Course"
-                          value={editingAssignmentCourse}
-                          onChange={(e) =>
-                            setEditingAssignmentCourse(e.target.value)
-                          }
-                        />
-                      </TableCell>
-                      <TableCell>
-                        <Input
-                          placeholder="Assignment"
-                          value={editingAssignmentName}
-                          onChange={(e) =>
-                            setEditingAssignmentName(e.target.value)
-                          }
-                        />
-                      </TableCell>
-                      <TableCell>
-                        <Popover>
-                          <PopoverTrigger asChild>
-                            <Button
-                              variant={"outline"}
-                              className={cn(
-                                "w-[280px] justify-start text-left font-normal",
-                                !editingAssignmentDate &&
-                                  "text-muted-foreground",
-                              )}
-                            >
-                              <CalendarIcon className="mr-2 h-4 w-4" />
-                              {editingAssignmentDate ? (
-                                format(editingAssignmentDate, "PPP")
-                              ) : (
-                                <span>Due date</span>
-                              )}
-                            </Button>
-                          </PopoverTrigger>
-                          <PopoverContent className="w-auto p-0">
-                            <Calendar
-                              mode="single"
-                              selected={editingAssignmentDate}
-                              onSelect={(e) =>
-                                setEditingAssignmentDate(e ?? new Date())
-                              }
-                              initialFocus
-                            />
-                          </PopoverContent>
-                        </Popover>
-                      </TableCell>
-                      <TableCell>
-                        <Select
-                          value={editingAssignmentStatus}
-                          onValueChange={(v) =>
-                            setEditingAssignmentStatus(v as Status)
-                          }
-                        >
-                          <SelectTrigger>
-                            <SelectValue placeholder="Status" />
-                          </SelectTrigger>
-                          <SelectContent>
-                            <SelectGroup>
-                              <SelectLabel>Status</SelectLabel>
-                              <SelectItem value="NOT_DONE">NOT DONE</SelectItem>
-                              <SelectItem value="IN_PROGRESS">
-                                IN PROGRESS
-                              </SelectItem>
-                              <SelectItem value="DONE">DONE</SelectItem>
-                            </SelectGroup>
-                          </SelectContent>
-                        </Select>
-                      </TableCell>
-                      <TableCell>
-                        <span className="flex gap-2">
-                          <Button
-                            onClick={async () => {
-                              setEditingAssignmentId("");
+                // if (row.original.id === editingAssignmentId) {
+                //   return (
+                //     <TableRow key={row.original.id}>
+                //       <TableCell>
+                //         <Input
+                //           placeholder="Course"
+                //           value={editingAssignmentCourse}
+                //           onChange={(e) =>
+                //             setEditingAssignmentCourse(e.target.value)
+                //           }
+                //         />
+                //       </TableCell>
+                //       <TableCell>
+                //         <Input
+                //           placeholder="Assignment"
+                //           value={editingAssignmentName}
+                //           onChange={(e) =>
+                //             setEditingAssignmentName(e.target.value)
+                //           }
+                //         />
+                //       </TableCell>
+                //       <TableCell>
+                //         <Popover>
+                //           <PopoverTrigger asChild>
+                //             <Button
+                //               variant={"outline"}
+                //               className={cn(
+                //                 "w-[280px] justify-start text-left font-normal",
+                //                 !editingAssignmentDate &&
+                //                   "text-muted-foreground",
+                //               )}
+                //             >
+                //               <CalendarIcon className="mr-2 h-4 w-4" />
+                //               {editingAssignmentDate ? (
+                //                 format(editingAssignmentDate, "PPP")
+                //               ) : (
+                //                 <span>Due date</span>
+                //               )}
+                //             </Button>
+                //           </PopoverTrigger>
+                //           <PopoverContent className="w-auto p-0">
+                //             <Calendar
+                //               mode="single"
+                //               selected={editingAssignmentDate}
+                //               onSelect={(e) =>
+                //                 setEditingAssignmentDate(e ?? new Date())
+                //               }
+                //               initialFocus
+                //             />
+                //           </PopoverContent>
+                //         </Popover>
+                //       </TableCell>
+                //       <TableCell>
+                //         <Select
+                //           value={editingAssignmentStatus}
+                //           onValueChange={(v) =>
+                //             setEditingAssignmentStatus(v as Status)
+                //           }
+                //         >
+                //           <SelectTrigger>
+                //             <SelectValue placeholder="Status" />
+                //           </SelectTrigger>
+                //           <SelectContent>
+                //             <SelectGroup>
+                //               <SelectLabel>Status</SelectLabel>
+                //               <SelectItem value="NOT_DONE">NOT DONE</SelectItem>
+                //               <SelectItem value="IN_PROGRESS">
+                //                 IN PROGRESS
+                //               </SelectItem>
+                //               <SelectItem value="DONE">DONE</SelectItem>
+                //             </SelectGroup>
+                //           </SelectContent>
+                //         </Select>
+                //       </TableCell>
+                //       <TableCell>
+                //         <span className="flex gap-2">
+                //           <Button
+                //             onClick={async () => {
+                //               setEditingAssignmentId("");
 
-                              await editAssignment({
-                                semesterId: semester.id,
-                                assignmentId: editingAssignmentId,
-                                data: {
-                                  course: editingAssignmentCourse,
-                                  name: editingAssignmentName,
-                                  dueDate: editingAssignmentDate,
-                                  status: editingAssignmentStatus,
-                                  link: null,
-                                },
-                              });
+                //               await editAssignment({
+                //                 semesterId: semester.id,
+                //                 assignmentId: editingAssignmentId,
+                //                 data: {
+                //                   course: editingAssignmentCourse,
+                //                   name: editingAssignmentName,
+                //                   dueDate: editingAssignmentDate,
+                //                   status: editingAssignmentStatus,
+                //                   link: null,
+                //                 },
+                //               });
 
-                              refetch();
-                            }}
-                          >
-                            Save
-                          </Button>
-                          <Button
-                            variant={"secondary"}
-                            onClick={() => setEditingAssignmentId("")}
-                          >
-                            Cancel
-                          </Button>
-                        </span>
-                      </TableCell>
-                    </TableRow>
-                  );
-                }
+                //               refetch();
+                //             }}
+                //           >
+                //             Save
+                //           </Button>
+                //           <Button
+                //             variant={"secondary"}
+                //             onClick={() => setEditingAssignmentId("")}
+                //           >
+                //             Cancel
+                //           </Button>
+                //         </span>
+                //       </TableCell>
+                //     </TableRow>
+                //   );
+                // }
 
                 return (
                   <TableRow
