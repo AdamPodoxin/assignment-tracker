904a64770550e81a5da1ede61cf7b91e6385de9d
Adam Podoxin <adam.podoxin@gmail.com>
2025-01-10 22:35:05 -0800

    Modal for new assignment

--- src/app/semester/[id]/AssignmentsTable.tsx
+++ src/app/semester/[id]/AssignmentsTable.tsx
@@ -41,6 +41,15 @@ import {
 } from "~/components/ui/popover";
 import { Input } from "~/components/ui/input";
 import { Calendar } from "~/components/ui/calendar";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "~/components/ui/dialog";
 import { type SemesterWithAssignments } from "~/hooks/useSemester";
 import getColumns from "./columns";
 
@@ -97,27 +106,89 @@ export const AssignmentsTable = ({
     },
   });
 
-  const [isNewAssignmentRowVisible, setIsNewAssignmentRowVisible] =
-    useState(false);
-
   const [newAssignmentCourse, setNewAssignmentCourse] = useState("");
   const [newAssignmentName, setNewAssignmentName] = useState("");
   const [newAssignmentDate, setNewAssignmentDate] = useState(new Date());
-  const [newAssignmentStatus, setNewAssignmentStatus] = useState<Status>(
-    Status.NOT_DONE,
-  );
 
   return (
     <div className="flex w-full flex-col gap-2">
       <span className="flex gap-2">
-        <Button
-          className="my-2"
-          onClick={() => setIsNewAssignmentRowVisible(true)}
-        >
-          <span className="flex items-center">
-            <PlusIcon className="mr-2 h-4 w-4" /> Add assignment
-          </span>
-        </Button>
+        <Dialog>
+          <DialogTrigger>
+            <Button
+              className="my-2"
+              // onClick={() => setIsNewAssignmentRowVisible(true)}
+            >
+              <span className="flex items-center">
+                <PlusIcon className="mr-2 h-4 w-4" /> Add assignment
+              </span>
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>New Assignment</DialogTitle>
+            </DialogHeader>
+            <div className="flex flex-col gap-2">
+              <Input
+                placeholder="Course"
+                value={newAssignmentCourse}
+                onChange={(e) => setNewAssignmentCourse(e.target.value)}
+              />
+              <Input
+                placeholder="Name"
+                value={newAssignmentName}
+                onChange={(e) => setNewAssignmentName(e.target.value)}
+              />
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "w-[280px] justify-start text-left font-normal",
+                      !newAssignmentDate && "text-muted-foreground",
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {newAssignmentDate ? (
+                      format(newAssignmentDate, "PPP")
+                    ) : (
+                      <span>Due date</span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={newAssignmentDate}
+                    onSelect={(e) => setNewAssignmentDate(e ?? new Date())}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button
+                  onClick={async () => {
+                    await addAssignment({
+                      assignment: {
+                        course: newAssignmentCourse,
+                        name: newAssignmentName,
+                        dueDate: newAssignmentDate,
+                        status: "NOT_DONE",
+                      },
+                      semesterId: semester.id,
+                    });
+
+                    refetch();
+                  }}
+                >
+                  Save
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
 
         <Button
           variant={"secondary"}
@@ -151,99 +222,6 @@ export const AssignmentsTable = ({
             ))}
           </TableHeader>
           <TableBody>
-            {isNewAssignmentRowVisible && (
-              <TableRow>
-                <TableCell>
-                  <Input
-                    placeholder="Course"
-                    value={newAssignmentCourse}
-                    onChange={(e) => setNewAssignmentCourse(e.target.value)}
-                  />
-                </TableCell>
-                <TableCell>
-                  <Input
-                    placeholder="Assignment"
-                    value={newAssignmentName}
-                    onChange={(e) => setNewAssignmentName(e.target.value)}
-                  />
-                </TableCell>
-                <TableCell>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "w-[280px] justify-start text-left font-normal",
-                          !newAssignmentDate && "text-muted-foreground",
-                        )}
-                      >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {newAssignmentDate ? (
-                          format(newAssignmentDate, "PPP")
-                        ) : (
-                          <span>Due date</span>
-                        )}
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0">
-                      <Calendar
-                        mode="single"
-                        selected={newAssignmentDate}
-                        onSelect={(e) => setNewAssignmentDate(e ?? new Date())}
-                        initialFocus
-                      />
-                    </PopoverContent>
-                  </Popover>
-                </TableCell>
-                <TableCell>
-                  <Select
-                    onValueChange={(v) => setNewAssignmentStatus(v as Status)}
-                  >
-                    <SelectTrigger>
-                      <SelectValue placeholder="Status" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectLabel>Status</SelectLabel>
-                        <SelectItem value="NOT_DONE">NOT DONE</SelectItem>
-                        <SelectItem value="IN_PROGRESS">IN PROGRESS</SelectItem>
-                        <SelectItem value="DONE">DONE</SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </TableCell>
-                <TableCell>
-                  <span className="flex gap-2">
-                    <Button
-                      onClick={async () => {
-                        setIsNewAssignmentRowVisible(false);
-
-                        await addAssignment({
-                          assignment: {
-                            course: newAssignmentCourse,
-                            name: newAssignmentName,
-                            dueDate: newAssignmentDate,
-                            status: newAssignmentStatus,
-                          },
-                          semesterId: semester.id,
-                        });
-
-                        refetch();
-                      }}
-                    >
-                      Save
-                    </Button>
-                    <Button
-                      variant={"secondary"}
-                      onClick={() => setIsNewAssignmentRowVisible(false)}
-                    >
-                      Cancel
-                    </Button>
-                  </span>
-                </TableCell>
-              </TableRow>
-            )}
-
             {table.getRowModel().rows?.length ? (
               table.getRowModel().rows.map((row) => {
                 if (row.original.id === editingAssignmentId) {
